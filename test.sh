d8893b7f74dc0935f59f81deef877f7400bfbbcb
Jesser <jssrpalma3@gmail.com>
2025-05-23 05:08:01 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,12 @@
 touch den/test{01..10}.txt;
 touch den/img{01..10}.png;
 
-echo "Test" | tee den/test{01..10}.txt 
+echo "Test" | tee den/test{01..10}.txt
+
+python -m venv venv
+
+source venv/bin/activate
+
+pip install -r requirements.txt
 
 python -m main
